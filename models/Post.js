0136f114731b3784f02e1bff2f77522571fbd145
Mayuran <mayuranswee23@gmail.com>
2022-07-17 16:31:27 -0400

    added voting system

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,33 @@
 const { Model, DataTypes } =  require("sequelize");
 const sequelize = require('../config/connection')
 
-class Post extends Model {}
+class Post extends Model {
+    static upvote(body, models){
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        })
+        .then(()=> {
+            // find post that had been voted on
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id', 
+                    'post_url', 
+                    'title', 
+                    'created_at', 
+                    //use MYSQL agregrate fxn query to get count of votes the post has and return it under 'vote_count'
+                [
+                    sequelize.literal(`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`),
+                    'vote_count'
+                ]
+                ]
+            })
+        })
+    }
+}
 
 //create the fields and columns for the Post Model
 
