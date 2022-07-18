b5abca5c712baf9c65f2590a3eafc20c8c200d47
Mayuran <mayuranswee23@gmail.com>
2022-07-17 22:42:35 -0400

    added functionality to add comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router(); 
-const { Post, User, Vote } = require('../../models'); 
+const { Post, User, Vote, Comment } = require('../../models'); 
 const sequelize = require('../../config/connection');
 
 //get all users
@@ -12,6 +12,14 @@ router.get('/', (req, res)=> {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'], 
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                } 
+            },
             {
                 model: User, 
                 attributes: ['username']
@@ -33,6 +41,15 @@ router.get('/:id', (req, res)=> {
         [sequelize.literal('(SELECT COUNT (*) FROM vote where post.id = vote.post_id)'), 'vote_count']
     ],
         include: [
+            {
+                model: Comment, 
+                // attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'], 
+                attributes: {exclude: ['updatedAt']},
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                } 
+            },
             {
                 model: User, 
                 attributes: ['username']
